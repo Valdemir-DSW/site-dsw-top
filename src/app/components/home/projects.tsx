ba0f9f2f727abb0eb62873113a2a262fc26b15ba
Valdemir 《DSW project》 <134114016+Valdemir-DSW@users.noreply.github.com>
2024-11-23 13:17:52 -0300

    Update projects.tsx

--- src/app/components/home/projects.tsx
+++ src/app/components/home/projects.tsx
@@ -10,37 +10,37 @@ export const ProjectsHome = () => {
         <ProjectItemHome
           title="DSW"
           description="Programa de desenvolvimento de volantes caseiros com force feedback no Arduino Leonardo."
-          url="https://github.com/Valdemir-DSW"
+          url="/downloads"
           status="FINALIZADO"
         />
         <ProjectItemHome
           title="DSW Button Box"
           description="Programa para fazer button box com Arduino Leonardo."
-          url="https://github.com/Valdemir-DSW"
+          url="/downloads"
           status="PARADO"
         />
         <ProjectItemHome
           title="DSW Painel Pro"
           description="Programa completo para extração de telemetria de jogos de corrida, com suporte para Arduinos e plataformas de movimento."
-          url="https://github.com/Valdemir-DSW"
+          url="/downloads"
           status="NAO LANCADO"
         />
         <ProjectItemHome
           title="DSW Pro"
           description="Programa gratuito para desenvolver volantes com force feedback na STM32."
-          url="https://github.com/Valdemir-DSW"
+          url="/downloads"
           status="FINALIZADO"
         />
         <ProjectItemHome
           title="DSW Button Box Pro"
           description="Atualização otimizada de seu antecessor, com melhor interface e sem travamentos, ainda gratuito."
-          url="https://github.com/Valdemir-DSW"
+          url="/downloads"
           status="FINALIZADO"
         />
         <ProjectItemHome
           title="DSW Gamepad Test"
           description="Teste seu volante ou gamepad."
-          url="https://github.com/Valdemir-DSW"
+          url="/downloads"
           status="DISPONIVEL"
         />
         <ProjectItemHome
