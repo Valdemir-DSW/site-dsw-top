69ac287643bbe305c659c14869eff7816136112c
Valdemir 《DSW project》 <134114016+Valdemir-DSW@users.noreply.github.com>
2024-11-26 14:44:49 -0300

    Update introduction.tsx

--- src/app/components/home/introduction.tsx
+++ src/app/components/home/introduction.tsx
@@ -15,11 +15,16 @@ export const IntroductionHome = () => {
         caros. Nosso foco é tornar a simulação de corrida acessível a todos,
         promovendo a criatividade e o aprendizado dentro da comunidade de jogos
         de corrida.
+        nosso oferecemos Total suporte no discord entre no botão abaixo
       </p>
       <div className="mt-5 lg:mt-7">
-        <Link href="/downloads">
-          <Button>Download</Button>
+        <Link href="https://discord.gg/ecf7PGTVVg">
+          <Button>DISCORD</Button>
         </Link>
+        <p className="text-center font-normal text-[--text-overshadowed] lg:w-3/6 lg:text-lg">
+        também temos tutoriais no YouTube você pode conferir os links abaixo e conferir
+        também nossos programas e ferramentas online olha a página!
+      </p>
       </div>
     </div>
   );
