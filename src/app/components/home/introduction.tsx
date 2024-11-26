6e54e38ebcd6a160bf7ca4ed6adae1a532f802c4
Valdemir 《DSW project》 <134114016+Valdemir-DSW@users.noreply.github.com>
2024-11-26 14:46:59 -0300

    Update introduction.tsx

--- src/app/components/home/introduction.tsx
+++ src/app/components/home/introduction.tsx
@@ -21,11 +21,12 @@ export const IntroductionHome = () => {
         <Link href="https://discord.gg/ecf7PGTVVg">
           <Button>DISCORD</Button>
         </Link>
-        <p className="text-center font-normal text-[--text-overshadowed] lg:w-3/6 lg:text-lg">
+        
+      </div>
+      <p className="text-center font-normal text-[--text-overshadowed] lg:w-3/6 lg:text-lg">
         também temos tutoriais no YouTube você pode conferir os links abaixo e conferir
         também nossos programas e ferramentas online olha a página!
       </p>
-      </div>
     </div>
   );
 };
