788de234fc8c5806cd4f15c283bd80917ebc2541
Valdemir 《DSW project》 <134114016+Valdemir-DSW@users.noreply.github.com>
2024-11-27 12:35:29 -0300

    Update projects.tsx

--- src/app/components/downloads/projects.tsx
+++ src/app/components/downloads/projects.tsx
@@ -9,34 +9,34 @@ export const ProjectsDownloads = () => {
         <ProjectItemDownload
           title="DSW BUTTON BOX PRO"
           updateHistory="Versão Firmware 1.41 — 26/08/2024: Correção de conflito nos analógicos 3 e 4. Caso encontre problemas, informe á ferramenta de relatório de bugs na aba 'Sobre'. A função wireless ainda está em desenvolvimento.."
-          credits="Valdemir"
+          credits="2024 Valdemir do DSW"
           urlDownload="https://drive.usercontent.google.com/download?id=19LYOJu8YpEkC1aB4EFmfJIRspdoxR6GX&export=download&authuser=0"
         />
 
         <ProjectItemDownload
           title="DSW"
           updateHistory="Versão 1/8/2024: Ajuste de compatibilidade com Windows 11."
-          credits="Valdemir"
+          credits="2023 Valdemir do DSW"
           urlDownload="https://drive.google.com/file/d/1YXFUSyRE2AWyxrU9cIi9hvI1CEAW8DYR/view?usp=sharing"
         />
         <ProjectItemDownload
           title="DSW hex to Arduino"
           updateHistory="N/A"
-          credits="Valdemir"
+          credits="2024 Valdemir do DSW"
           urlDownload="https://drive.google.com/uc?export=download&id=1FM6WTYsfnZAgtsX0peS9I7DZ-0QMbzWv"
         />
 
         <ProjectItemDownload
           title="DSW Lite"
           updateHistory="Versão 1/8/2024: Ajuste de compatibilidade com windows 11."
-          credits="Valdemir"
+          credits="2022 Valdemir do DSW"
           urlDownload="https://www.mediafire.com/file/liphk6nk52j5zv0/DSW_lite_V5.zip/file"
         />
 
         <ProjectItemDownload
           title="EMC lite"
           updateHistory="N/A"
-          credits="Desconhecido"
+          credits="EMC Development"
           urlDownload="https://drive.google.com/drive/folders/1_bqvdywVJx2uescXi4YgN1H_9-k0qUPQ"
         />
 
@@ -69,7 +69,7 @@ export const ProjectsDownloads = () => {
         <ProjectItemDownload
           title="DSW button box"
           updateHistory="N/A"
-          credits="Valdemir"
+          credits="2022 Valdemir do DSW"
           urlDownload="https://www.mediafire.com/file/gvf8hbtkxcdls28/Button_box.zip/file"
         />
       </div>
