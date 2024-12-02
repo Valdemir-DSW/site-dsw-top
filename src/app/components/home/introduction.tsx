b9ba586768638f0e3825fcea3868b359b85b21b6
Valdemir 《DSW project》 <134114016+Valdemir-DSW@users.noreply.github.com>
2024-12-02 15:15:12 -0300

    Update introduction.tsx

--- src/app/components/home/introduction.tsx
+++ src/app/components/home/introduction.tsx
@@ -8,7 +8,7 @@ export const IntroductionHome = () => {
         Bem-vindo ao DSW Simuladores
       </h1>
       <p className="text-center font-normal text-[--text-overshadowed] lg:w-3/6 lg:text-lg">
-    O projeto DSW (DIY Steering Wheel) tem como objetivo desenvolver simuladores de direção de alta qualidade, acessíveis e de baixo custo. Começando com a necessidade de softwares mais específicos e versáteis para meu simulador, criei um grupo para compartilhar o conhecimento adquirido. A galera gostou, e continuei desenvolvendo softwares cada vez mais avançados e intuitivos. Esses simuladores são voltados para entusiastas de corridas virtuais que buscam uma experiência imersiva sem precisar investir em equipamentos caros. Nosso foco é tornar a simulação de corrida acessível a todos, promovendo criatividade e aprendizado dentro da comunidade de jogos de corrida. Oferecemos suporte total no Discord, acesse pelo botão abaixo.
+    O projeto DSW (DIY Steering Wheel) tem como objetivo desenvolver simuladores de direção de alta qualidade, acessíveis e de baixo custo. Começando com a necessidade de softwares mais específicos e versáteis para meu simulador(Valdemir), criei um grupo para compartilhar o conhecimento adquirido. A galera gostou, e continuei desenvolvendo softwares cada vez mais avançados e intuitivos. Esses simuladores são voltados para entusiastas de corridas virtuais que buscam uma experiência imersiva sem precisar investir em equipamentos caros. Nosso foco é tornar a simulação de corrida acessível a todos, promovendo criatividade e aprendizado dentro da comunidade de jogos de corrida. Oferecemos suporte total no Discord, acesse pelo botão abaixo.
 </p>
       <div className="mt-5 lg:mt-7">
         <Link href="https://discord.gg/ecf7PGTVVg">
